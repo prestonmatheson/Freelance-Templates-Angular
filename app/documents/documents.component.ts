8a789b3b8da8db8d8c11f50ed1da8b4552038fbd
Preston Matheson <pmphoto123@gmail.com>
2017-04-17 10:34:14 -0600

    changed how the title is being called

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -7,6 +7,7 @@ import { Document } from './document';
   templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
+  pageTitle: string = "Document Dashboard"
   documents: Document[] = [
     {
       title: 'My First Doc',
